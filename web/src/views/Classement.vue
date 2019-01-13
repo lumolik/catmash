7c4151ce2bea764318dc6cea79e46476814b43e2
ludovic meril <ludovic.meril@gmail.com>
2019-01-13 18:51:56 +0100

    initial commit

---
+++ web/src/views/Classement.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="classement">
+    <h1>page de classement</h1>
+  </div>
+</template>
---
+++ web/src/views/Vote.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="vote"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {}
+</script>
